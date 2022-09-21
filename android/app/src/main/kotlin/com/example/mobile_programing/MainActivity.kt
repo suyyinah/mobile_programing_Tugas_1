11500933b31825679b5e2ccadf46b8dcf6c3e070
SUYYINAH <Suyyinahnabila28@gmail.com>
2022-09-21 07:22:37 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mobile_programing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_programing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import './widgets/soal_1.dart';
+import './widgets/soal_2.dart';
+import './widgets/soal_3.dart';
+import './widgets/soal_4.dart';
+import './widgets/soal_5.dart';
+import './widgets/soal_6.dart';
+import './widgets/soal_7.dart';
+import './widgets/soal_8.dart';
+import './widgets/soal_9.dart';
+import './widgets/soal_10.dart';
+import './widgets/soal_11.dart';
+import './widgets/soal_12.dart';
+import './widgets/soal_13.dart';
+import './widgets/soal_14.dart';
+import './widgets/soal_15.dart';
+import './widgets/soal_16.dart';
+import './widgets/soal_17.dart';
+import './widgets/soal_18.dart';
+import './widgets/soal_19.dart';
+import './widgets/soal_20.dart';
+import './widgets/soal_21.dart';
+import './widgets/soal_22.dart';
+import './widgets/soal_23.dart';
+import './widgets/soal_24.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: soal_24(),
+    );
+  }
+}
